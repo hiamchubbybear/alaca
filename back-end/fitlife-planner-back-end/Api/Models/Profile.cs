313ce26d7846fec8b49abdd9f563dffe6861e0db
Chessy <ilovepakpak@gmail.com>
2025-11-30 00:14:20 +0700

    refactor profile model to update fields and remove unused request dto

--- back-end/fitlife-planner-back-end/Api/Models/Profile.cs
+++ back-end/fitlife-planner-back-end/Api/Models/Profile.cs
@@ -1,20 +1,39 @@
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
 using fitlife_planner_back_end.Api.Enums;
 
 namespace fitlife_planner_back_end.Api.Models;
 
 public class Profile
 {
-    [Key] public Guid Id = Guid.NewGuid();
-    public Guid UserId;
-    public string DisplayName;
-    public string AvatarUrl;
-    public DateTime BirthDate;
-    public Gender Gender;
-    public string Bio;
-    public DateTime CreateAt;
-    public DateTime UpdateAt;
-    public int Version;
+    [Key] public Guid ProfileId { get; set; }
+    public Guid UserId { get; set; }
+    public string DisplayName { get; set; }
+    public string AvatarUrl { get; set; }
+    public DateTime BirthDate { get; set; }
+    public Gender Gender { get; set; }
+    public string Bio { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+    public int Version { get; set; }
+    public ICollection<Post> Posts { get; set; }
 
+    public Profile()
+    {
+    }
+
+    public Profile(Guid userId, string displayName, string avatarUrl, DateTime birthDate, Gender gender, string bio)
+    {
+        ProfileId = Guid.NewGuid();
+        UserId = userId;
+        DisplayName = displayName;
+        AvatarUrl = avatarUrl;
+        BirthDate = birthDate;
+        Gender = gender;
+        Bio = bio;
+        Posts = new List<Post>();
+        CreateAt = DateTime.Now;
+        UpdateAt = DateTime.Now;
+        Version = 1;
+    }
 }
-
